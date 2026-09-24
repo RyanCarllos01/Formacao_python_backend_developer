6f019bea6ab1fe2c76ff6230dca07e39538bc85f
RyanCarllos01 <ryancarllos.negro@gmail.com>
2026-09-24 09:17:18 -0300

    feat: adiciona primeiros exercícios em Python

---
+++ 01_boas_praticas.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+frutas = ["pera", "maçã", "banana", "laranja"]
+print(frutas)
+
+carros = [
+    "ferrari",
+    "lamborghini",
+    "porsche",
+    "bugatti",
+    "mercedes",
+    "bmw",
+    "audi",
+    "volkswagen",
+    "ford",
+    "chevrolet",
+    "toyota",
+    "honda",
+    "nissan",
+    "hyundai",
+    "kia",
+    "mazda",
+    "subaru",
+    "volvo",
+    "jaguar",
+    "land rover",
+]
+
+print(carros)
